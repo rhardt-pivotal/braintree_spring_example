2c4334853ba245e50f908a1d2b070c44fd1db971
Rob Hardt <rhardt@pivotal.io>
2017-03-08 14:02:18 -0800

    logging

--- src/main/java/springexample/Application.java
+++ src/main/java/springexample/Application.java
@@ -3,13 +3,19 @@ package springexample;
 import java.io.File;
 import com.braintreegateway.BraintreeGateway;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
     public static String DEFAULT_CONFIG_FILENAME = "config.properties";
     public static BraintreeGateway gateway;
+
+    private static final Log log = LogFactory.getLog("BRAINTREE_DEBUG");
 
     public static void main(String[] args) {
         File configFile = new File(DEFAULT_CONFIG_FILENAME);
@@ -26,4 +32,18 @@ public class Application {
 
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public CommandLineRunner clr() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                log.info("IN CLR BLOCK");
+
+
+            }
+        };
+    }
+
+
 }
